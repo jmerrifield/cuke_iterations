9c2766a64a800b535f27389338ba84fef235ae80
Jon Merrifield <jon@jmerrifield.com>
2011-08-22 09:28:44 +0100

    Process excluded tags

--- lib/cuke_profiles/scenario_list_generator.rb
+++ lib/cuke_profiles/scenario_list_generator.rb
@@ -2,7 +2,10 @@ module CukeProfiles
   class ScenarioListGenerator
     class << self
       def for_profile(parsed_features, profile)
-        parsed_features.select { |f| (f[:tags] & profile[:include_tags]).any? }
+        parsed_features.select do |f|
+          next if (f[:tags] & profile[:exclude_tags]).any?
+          (f[:tags] & profile[:include_tags]).any?
+        end
       end
     end
   end
--- spec/scenario_list_generator_spec.rb
+++ spec/scenario_list_generator_spec.rb
@@ -5,18 +5,36 @@ describe "Scenario list generator" do
     @parsed_features = [
         @test1 = {filename: 'cuking_rocks.feature', line: 10, tags: ['@tag3', '@tag4']},
         @test2 = {filename: 'cuking_rocks.feature', line: 14, tags: ['@tag2', '@tag9']},
-        @test3 = {filename: 'cuking_rocks.feature', line: 18, tags: ['@tag8']},
+        @test3 = {filename: 'cuking_rocks.feature', line: 18, tags: ['@tag3', '@tag8']},
     ]
   end
 
   it "should include the scenarios that have tags specified in the profile" do
     profile = {
         name: "iphone",
-        include_tags: ['@tag2', '@tag8']
+        include_tags: ['@tag2', '@tag8'],
+        exclude_tags: []
+    }
+
+    profile2 = {
+        name: "blah"
+    }
+
+    puts [profile, profile2].to_yaml
+    list = ScenarioListGenerator.for_profile(@parsed_features, profile)
+
+    list.should =~ [@test2, @test3]
+  end
+
+  it "should exclude scenarios that the profile specifies to exclude" do
+    profile = {
+        name: "android",
+        include_tags: ['@tag3'],
+        exclude_tags: ['@tag8'],
     }
 
     list = ScenarioListGenerator.for_profile(@parsed_features, profile)
 
-    list.should =~ [@test2, @test3]
+    list.should =~ [@test1]
   end
 end
