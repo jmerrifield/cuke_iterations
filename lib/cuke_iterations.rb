5841006fb8b5cb9ce8aab679ca6ec758ab106cc6
Jon Merrifield <jon@jmerrifield.com>
2012-02-01 16:06:09 +0000

    Extract main logic into it's own class so it can be tested (eventually)

--- lib/cuke_iterations.rb
+++ lib/cuke_iterations.rb
@@ -1,7 +1,8 @@
 $:.push File.expand_path("../cuke_iterations", __FILE__)
 
 require 'version'
+require 'cucumber_helper'
 require 'cuke_parser'
 require 'scenario_extracting_formatter'
+require 'scenario_file_writer'
 require 'scenario_list_generator'
-require 'cucumber_helper'
---
+++ lib/cuke_iterations/scenario_file_writer.rb
@@ -0,0 +1,20 @@
+module CukeIterations
+  class ScenarioFileWriter
+    def write_scenarios(features_dir, iteration_file, out_file)
+      iterations = YAML.load(File.read(iteration_file))
+
+      all_scenarios = []
+      features = CukeParser.parse_features(features_dir)
+      iterations.each do |iteration_name, iteration|
+        iteration_dir = File.join(features_dir, 'iterations', iteration_name, '..', '..')
+        FileUtils.mkdir_p iteration_dir
+
+        ScenarioListGenerator.for_iteration(features, iteration).each do |scenario|
+          all_scenarios << File.join(iteration_dir, scenario[:filename] + ":#{scenario[:line]}")
+        end
+      end
+
+      File.open(out_file, 'w') { |f| f.puts all_scenarios }
+    end
+  end
+end
