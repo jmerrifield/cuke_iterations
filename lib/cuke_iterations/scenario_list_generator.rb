2b0eb73fc425454cabfd7d8da583cd886717864e
Jon Merrifield <jon@jmerrifield.com>
2012-02-01 14:14:54 +0000

    Refactor ScenarioListGenerator to be clearer

--- lib/cuke_iterations/scenario_list_generator.rb
+++ lib/cuke_iterations/scenario_list_generator.rb
@@ -1,11 +1,23 @@
 module CukeIterations
   class ScenarioListGenerator
     class << self
-      def for_iteration(parsed_features, iteration)
-        parsed_features.select do |f|
-          next if (f[:tags] & iteration[:exclude_tags]).any?
-          (f[:tags] & iteration[:include_tags]).any? || iteration[:include_tags] == []
+      def for_iteration(scenarios, iteration_info)
+        scenarios.select do |s|
+          next if is_excluded?(s, iteration_info)
+          is_included_by_tag?(s, iteration_info) || no_include_tags_specified?(iteration_info)
         end
+      end
+
+      def no_include_tags_specified?(iteration)
+        iteration[:include_tags] == []
+      end
+
+      def is_included_by_tag?(scenario, iteration)
+        (scenario[:tags] & iteration[:include_tags]).any?
+      end
+
+      def is_excluded?(scenario, iteration)
+        (scenario[:tags] & iteration[:exclude_tags]).any?
       end
     end
   end
