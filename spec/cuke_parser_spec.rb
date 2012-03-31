daa367a3693d31dee43fb51fcfc717dbb02cc6d9
Jon Merrifield <jon@jmerrifield.com>
2012-03-31 15:20:12 -0600

    RubyMine autoformat

--- spec/cuke_parser_spec.rb
+++ spec/cuke_parser_spec.rb
@@ -10,16 +10,16 @@ describe "Extracting scenarios" do
 
   it "should find all the scenarios and examples" do
     expected_features = [
-      {filename: 'cuking_rocks.feature', line: 10},
-      {filename: 'cuking_rocks.feature', line: 21},
-      {filename: 'cuking_rocks.feature', line: 22},
-      {filename: 'subdirectory/i_love_cukes.feature', line: 4},
-      {filename: 'subdirectory/i_love_cukes.feature', line: 15},
-      {filename: 'subdirectory/i_love_cukes.feature', line: 16},
-      {filename: 'subdirectory/i_love_cukes.feature', line: 25},
+        {filename: 'cuking_rocks.feature', line: 10},
+        {filename: 'cuking_rocks.feature', line: 21},
+        {filename: 'cuking_rocks.feature', line: 22},
+        {filename: 'subdirectory/i_love_cukes.feature', line: 4},
+        {filename: 'subdirectory/i_love_cukes.feature', line: 15},
+        {filename: 'subdirectory/i_love_cukes.feature', line: 16},
+        {filename: 'subdirectory/i_love_cukes.feature', line: 25},
     ]
 
-    parsed_files_with_lines = @parsed_features.map{|x| {filename: x[:filename], line: x[:line]}}
+    parsed_files_with_lines = @parsed_features.map { |x| {filename: x[:filename], line: x[:line]} }
     parsed_files_with_lines.should =~ expected_features
   end
 
@@ -28,32 +28,32 @@ describe "Extracting scenarios" do
 
     it "should read scenario-level tags" do
       @tag_info.should include filename: 'subdirectory/i_love_cukes.feature',
-        line: 4,
-        tags: ['@tag1', '@tag2']
+                               line: 4,
+                               tags: ['@tag1', '@tag2']
     end
 
     it "should read feature-level tags on scenarios" do
       @tag_info.should include filename: 'cuking_rocks.feature',
-        line: 10,
-        tags: ['@feature_tag1', '@feature_tag2']
+                               line: 10,
+                               tags: ['@feature_tag1', '@feature_tag2']
     end
 
     it "should read scenario-level tags on example rows" do
       @tag_info.should include filename: 'subdirectory/i_love_cukes.feature',
-        line: 15,
-        tags: ['@tag3', '@tag4']
+                               line: 15,
+                               tags: ['@tag3', '@tag4']
     end
 
     it "should read feature-level tags on example rows" do
       @tag_info.should include filename: 'cuking_rocks.feature',
-        line: 21,
-        tags: ['@feature_tag1', '@feature_tag2']
+                               line: 21,
+                               tags: ['@feature_tag1', '@feature_tag2']
     end
 
     it "should read example-group tags on example rows" do
       @tag_info.should include filename: 'subdirectory/i_love_cukes.feature',
-        line: 25,
-        tags: ['@tag5', '@tag6', '@tag7', '@tag8']
+                               line: 25,
+                               tags: ['@tag5', '@tag6', '@tag7', '@tag8']
     end
   end
 
@@ -62,15 +62,15 @@ describe "Extracting scenarios" do
 
     it "should read the names of scenarios" do
       @scenario_info.should include filename: 'subdirectory/i_love_cukes.feature',
-              line: 4,
-              scenario_name: 'Scenario with tags'
+                                    line: 4,
+                                    scenario_name: 'Scenario with tags'
     end
 
     it "should read the outline name and row details of example rows" do
       @scenario_info.should include filename: 'cuking_rocks.feature',
-              line: 22,
-              scenario_name: 'My scenario outline',
-              example_row: {'cukes' => '11', 'colour' => 'red'}
+                                    line: 22,
+                                    scenario_name: 'My scenario outline',
+                                    example_row: {'cukes' => '11', 'colour' => 'red'}
     end
   end
 end
